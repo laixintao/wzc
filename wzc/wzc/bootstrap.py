8a46c59476f9e53819073cf3c95e7bd633a716ae
赖信涛 <laixintao1995@163.com>
2017-04-09 13:12:38 +0800

    server

--- wzc/wzc/bootstrap.py
+++ wzc/wzc/bootstrap.py
@@ -1,17 +1,27 @@
 # -*- coding: utf-8 -*-
 import sys
-sys.path.append("..")
+from os.path import abspath, join, dirname
+
+wzc_fir = join(abspath(dirname('__file__')), '../../wzc')
+sys.path.insert(0, wzc_fir)
+
 from spider.tornado_fetcher import Fetcher
+from settings import PHANTOM_SERVER, BASE_URL
+
 
 def start_job():
     fetcher = Fetcher(
         user_agent='phantomjs',  # user agent
-        phantomjs_proxy='http://localhost:12306',  # phantomjs url
+        phantomjs_proxy=PHANTOM_SERVER,  # phantomjs url
         pool_size=10,  # max httpclient num
         async=False
     )
-    content = fetcher.phantomjs_fetch('http://python-china.org')
+    content = fetcher.phantomjs_fetch(BASE_URL)
     print content.keys()
     print content['content']
     with open('download.html', 'w') as f:
         f.write(content['content'].encode('utf-8'))
+
+
+if __name__ == '__main__':
+    start_job()
---
+++ wzc/wzc/settings.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+SERVER_PORT = 8082
+PHANTOMJS_PORT = 9989
+PHANTOMJS_HOST = 'http://localhost'
+PHANTOM_SERVER = '{host:port}'.format(host=PHANTOMJS_HOST, port=PHANTOMJS_PORT)
+BASE_URL = 'http://python-china.org'
