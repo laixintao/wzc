bde7e707747599c83ceda06ad5b97c673bcfd7cb
赖信涛 <laixintao1995@163.com>
2017-04-10 22:57:54 +0800

    storage is ok

--- wzc/spider/__init__.py
+++ wzc/spider/__init__.py
@@ -1,1 +1,21 @@
 # -*- coding: utf-8 -*-
+from tornado_fetcher import Fetcher
+from storage import MongodbStorage
+from wzc.wzc.settings import HTML_PATH
+
+
+def update_url(url):
+    '''
+    :param url:
+    :return: res [u'cookies', u'url', u'orig_url', u'time', u'content', u'headers', u'status_code', u'js_script_result']
+    '''
+    fetcher = Fetcher()
+    res = fetcher.phantomjs_fetch(url)
+    mongo = MongodbStorage()
+    filename = mongo.save(res)
+    with open('{}{}.html'.format(HTML_PATH, filename), 'w') as f:
+        f.write(res['content'].encode('utf-8'))
+
+
+if __name__ == '__main__':
+    update_url('http://www.china-python.org')
--- wzc/spider/storage.py
+++ wzc/spider/storage.py
@@ -1,24 +1,27 @@
 # -*- coding: utf-8 -*-
 
 import pymongo
+import hashlib
 
 
 class MongodbStorage(object):
     def __init__(self):
         self.page_info = {}
         conn = pymongo.MongoClient()
-        self.db = conn['hello']['world']
+        self.db = conn['wzc']['page']
 
     def save(self, result):
         '''
         Saving html to file, and other infomation to mongo.
         use a md5 code stored in mongo to located file.
         :param result: key response
-        :return:
+        :return: md5 string
         '''
         html = result.get('content')
-        
-        self.db.insert(result)
+        hash_md5 = hashlib.md5(html.encode('utf-8')).hexdigest()
+        result['md5'] = hash_md5
+        self.db.insert(result) #TODO update
+        return hash_md5
 
 
 if __name__ == '__main__':
